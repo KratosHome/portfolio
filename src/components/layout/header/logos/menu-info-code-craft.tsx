83a8052fdd1dc425f89b69addb2abe2759aff340
olegtkach <olegontkach101@gmail.com>
2025-02-23 13:38:57 +0200

    fix: logo

--- src/components/layout/header/logos/menu-info-code-craft.tsx
+++ src/components/layout/header/logos/menu-info-code-craft.tsx
@@ -10,8 +10,10 @@ const MenuInfoCodeCraft = () => {
       className="relative flex gap-2 font-bold uppercase"
     >
       <Image src={'/logo.svg'} alt={'logo'} width="40" height="40" />
-      <span> code craft</span>
-      <span className="absolute -bottom-2 right-0 text-[10px]">master</span>
+      <div className="flex flex-col justify-center font-bold">
+        <div className="text-[20px] md:text-[25px]"> code craft</div>
+        <div className="absolute -bottom-1 right-0 text-[10px]">master</div>
+      </div>
     </Link>
   )
 }
